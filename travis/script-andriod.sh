d752456642b84f053158bd1f9be2911286434293
Paul Walger <metaxy@walger.name>
2018-01-17 14:21:14 +0100

    fix path for andriod

--- travis/script-andriod.sh
+++ travis/script-andriod.sh
@@ -1,9 +1,9 @@
-
 source qt-5.5.1-android-19.env
 which qmake
 
 qmake
-make install INSTALL_ROOT=$WORKDIR/build
-androiddeployqt --output $WORKDIR/build
-ls
+make
+make install INSTALL_ROOT=./build
+androiddeployqt --output ./build
+ls build/
 
